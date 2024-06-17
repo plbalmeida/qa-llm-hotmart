38c3ef98f3fec56207a37d9eeb5f555a1a5effe2
plbalmeida <plbalmeida@usp.br>
2024-06-16 22:15:34 -0300

    fix(.github/workflows/ci-cd.yml): ajustes para inclusão das secrets da Open AI e do Pinecone

--- src/api/tests/test_app.py
+++ src/api/tests/test_app.py
@@ -18,12 +18,10 @@ def client():
     os.unlink(app.config['DATABASE'])
 
 
-@patch('src.api.app.retrieve')
-@patch('src.api.app.client.chat.completions.create')
+@patch.dict(os.environ, {'OPENAI_API_KEY': 'test_openai_key', 'PINECONE_API_KEY': 'test_pinecone_key'})  # noqa 401
+@patch('api.app.retrieve')
+@patch('api.app.client.chat.completions.create')
 def test_qa(mock_create, mock_retrieve, client):
-    """
-    Testa a rota de perguntas e respostas, verificando se a resposta é gerada corretamente.
-    """  # noqa 401
     mock_retrieve.return_value = ["document 1", "document 2", "document 3"]
     mock_create.return_value = MagicMock(choices=[MagicMock(message=MagicMock(content="Test answer"))])  # noqa 401
 
@@ -32,20 +30,16 @@ def test_qa(mock_create, mock_retrieve, client):
     assert response.json == {'answer': 'Test answer'}
 
 
+@patch.dict(os.environ, {'OPENAI_API_KEY': 'test_openai_key', 'PINECONE_API_KEY': 'test_pinecone_key'})  # noqa 401
 def test_qa_no_question(client):
-    """
-    Testa a rota de perguntas e respostas sem fornecer uma pergunta, esperando um erro 400.
-    """  # noqa 401
     response = client.post('/qa', json={})
     assert response.status_code == 400
     assert response.json == {'error': 'Question is required'}
 
 
-@patch('src.api.app.retrieve')
+@patch.dict(os.environ, {'OPENAI_API_KEY': 'test_openai_key', 'PINECONE_API_KEY': 'test_pinecone_key'})  # noqa 401
+@patch('api.app.retrieve')
 def test_qa_no_relevant_embeddings(mock_retrieve, client):
-    """
-    Testa a rota de perguntas e respostas sem encontrar embeddings relevantes, esperando um erro 404.
-    """  # noqa 401
     mock_retrieve.return_value = []
 
     response = client.post('/qa', json={'question': 'Test question'})
